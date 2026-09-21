1c6c2206e03e621772cb04784e0c56f841a8d28c
Arthur <arthurandriollihl1@gmail.com>
2026-09-20 23:44:13 -0400

    ...

--- Lista_III/Exerc_6.c
+++ Lista_III/Exerc_6.c
@@ -1,6 +1,5 @@
 //Escreva uma função que procure a ocorrência de um vetor menor dentro de um vetor
 //maior.
-
 //-Assinatura: int* busca_subvetor(int *vetor, int tam_v, int *sub, int tam_s);
 //A função deve procurar se a sequência exata de números do vetor sub existe
 //dentro de vetor. Se encontrar, retorne um ponteiro apontando para o início dessa
@@ -12,9 +11,38 @@ int *buscar_subvetor(int *vetor, int tam_v, int *sub, int tam_s);
 int main(int argc, char **argv) {
     int tamV = 10;
     int tamSV = 4;
-    int vetor[tamV] = [1,2,3,4,5,6,7,8,9,10];
-    int subvetor[tamSV] = [4,5,6,7];
+    int vetor[10] = {1,2,3,4,5,6,7,8,9,10};
+    int subvetor[4] = {4,5,6,7};
 
-    int buscar_subvetor(vetor, tamV, subvetor, tamSV);
+    int *resultado = buscar_subvetor(vetor, tamV, subvetor, tamSV);
 
+    if (resultado != NULL)
+    {
+        printf("Subvetor encontrado!\n");
+        printf("Comeca no valor: %d\n", *resultado);
+    }
+    else
+    {
+        printf("Subvetor nao encontrado.\n");
+    }
+
+}
+
+int *buscar_subvetor(int *vetor, int tam_v, int *sub, int tam_s) {
+    for (int i = 0; i <= tam_v - tam_s; i++) {
+        int encontrou = 1;
+
+        for (int j = 0; j < tam_s; j++) {
+            if (vetor[i + j] != sub[j]) {
+                encontrou = 0;
+                break;
+            }
+        }
+
+        if (encontrou) {
+            return &vetor[i];
+        }
+    }
+
+    return NULL;
 }
